6fa7f44e68ad02316738efe453119225a9ad3f87
mihneamihailescu <mihneamihailescu@gmail.com>
2024-03-17 17:22:44 +0200

    Added rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveComponentComponent } from './reactive-component/reactive-component.component';
+import { AppModule } from './app.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
 export class AppComponent {
   title = 'angular-observable';
